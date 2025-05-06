e817e1121662da2dbddd22d959b7c2d37b362385
Akhil <akhilrapell18@gmail.com>
2025-05-06 09:14:57 +0530

    Updated

--- discountpri.py
+++ discountpri.py
@@ -1,19 +1,25 @@
 amount = float(input("Enter the amount : "))
-isPrimeMen = bool(input("Is a Prime Member : "))
+status = input("Is a Prime Member { Y for Yes } or {N for No} : ").upper()
+isPrimeMem = False
+if status == "Y":
+    isPrimeMem = True
 
 discount = 0
+
 if amount >= 100000:
-    discount = amount*0.2 #20%
+    discount = amount * 0.2 #20%
     print("20% Discount Applied : ",discount)
 elif amount >= 50000:
     discount = amount * 0.1 #10%
     print("10% Discount Applied : ",discount)
 elif amount >= 35000:
-    discont = amount * 0.05 #5%
+    discount = amount * 0.05 #5%
     print("5% Discount Applied : ",discount)
-if isPrimeMen and amount > 500 :
-    discount += amount * 0.05
+    
+if isPrimeMem and amount > 0 :
+    discount += amount * 0.05 #+5%
     print("Additional 5% Discount on Prime Member Applied Total Discount is : ",discount)
+    
 bill = amount - discount
     
 print("The Bill amount is : ",bill)
